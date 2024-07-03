890d5757763e972bf09e5498fc9a4ae7815f0b10
jhermosilla75 <jvrhermosilla@gmail.com>
2024-07-03 12:48:34 -0300

    primer comit del repositorio

---
+++ src/Jugador.java
@@ -0,0 +1,25 @@
+﻿public class Jugador {
+    private final int id;
+    private final int ranking;
+    private final long tiempoEspera;
+
+    public Jugador(int id, int ranking) {
+        this.id = id;
+        this.ranking = ranking;
+        this.tiempoEspera = System.currentTimeMillis(); 
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getRanking() {
+        return ranking;
+    }
+
+    public long getTiempoEspera() {
+        return tiempoEspera;
+    }
+
+    
+}
